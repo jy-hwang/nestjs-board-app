14a1a87e1b5bccf555dd8f0d16d723181b65b642
jy-hwang <stratojy@gmail.com>
2024-10-17 15:51:41 +0900

    feat. project 처음 커밋
    1. 소스와 함께 처음 커밋
    2. nestjs 로 만드는 게시판 프로젝트.

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  // @Get('name/:name')
+  // getName(@Param('name') name: string): string {
+  //   return `${name} hello`;
+  // }
+  @Get('name')
+  getName(@Query('name') name: string): string {
+    return `${name} hello`;
+  }
+}
